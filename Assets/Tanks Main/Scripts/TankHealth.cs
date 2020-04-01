3221626ebbc5a868b129df99fa4d904a487b6b4e
Joel Camilo Fernandes <torn3do64@gmail.com>
2020-04-01 18:21:29 +0100

    temp

--- Assets/Tanks Main/Scripts/TankHealth.cs
+++ Assets/Tanks Main/Scripts/TankHealth.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TankHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Variables
+    public float startingHealth = 100f;
+    public Slider healthSlider;
+    public Image fillImage;
+    public Color fullHealthColor = Color.green;
+    public Color zeroHealthColor = Color.red;
+    public GameObject explosionPrefab;
 
-    // Update is called once per frame
-    void Update()
+    AudioSource explosionAudioSource;
+    ParticleSystem explosionParticles;
+    float currentHealth;
+    bool isDead;
+    #endregion
+
+    private void Awake()
     {
-        
+        explosionParticles = Instantiate(explosionPrefab).GetComponent<ParticleSystem>();
+        explosionAudioSource = explosionParticles.GetComponent<AudioSource>();
+        explosionParticles.gameObject.SetActive(false);
+    }
+    private void OnEnable()
+    {
+        currentHealth = startingHealth;
+        isDead = false;
+        SetHealthUI();
+
+    }
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        SetHealthUI();
+        if(currentHealth<=0f && !isDead)
+        {
+            OnDeath();
+        }
+    }
+    void SetHealthUI()
+    {
+        healthSlider.value = currentHealth;
+        fillImage.color = Color.Lerp(zeroHealthColor, fullHealthColor, currentHealth / startingHealth);
+    }
+    void OnDeath()
+    {
+        isDead = true;
+        explosionParticles.transform.position = transform.position;
+        explosionParticles.gameObject.SetActive(true);
+        explosionParticles.Play();
+        explosionAudioSource.Play();
+        gameObject.SetActive(false);
     }
 }
